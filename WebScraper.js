d9666df3664feada2b6fe2acd808b398340b95da
RajPatel <raj191@live.com>
2021-10-12 21:36:22 -0400

    final changes

---
+++ WebScraper.js
@@ -0,0 +1,28 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const { response } = require('express');
+const express = require('express');
+const PORT = 8000;
+
+
+const app = express();
+const url ="https://www.theguardian.com/us";
+axios(url)
+    .then(response=>{
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const articles = [];
+        $('.fc-item__title',html).each(()=>{//this is grabbing the tags with this classname 
+            const title = $(this).text();//this is grabbing the text from the tags
+            const url = $(this).find('a').attr('href');//this is getting the url that the tags lead to on click 
+            articles.push({
+                title,
+                url
+            })//pushing this object of title and url into the array 
+        })
+        console.log(articles)
+
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));//listens to the port 
+
