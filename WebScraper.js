5a95cb96ec9eed4e5259d4f8ad8c85fb9643d149
RajPatel <raj191@live.com>
2021-10-12 21:59:21 -0400

    this is really the final change

--- WebScraper.js
+++ WebScraper.js
@@ -6,13 +6,13 @@ const PORT = 8000;
 
 
 const app = express();
-const url ="https://www.theguardian.com/us";//the urn to scrape from 
-axios(url)
+const URL ="https://www.theguardian.com/us";//the urn to scrape from 
+axios(URL)
     .then(response=>{
         const html = response.data;
         const $ = cheerio.load(html);
         const articles = [];
-        $('.u-faux-block-link__overlay js-headline-text',html).each(()=>{//this is grabbing the tags with this classname 
+        $('.fc-item__title',html).each(function(){//this is grabbing the tags with this classname 
             const title = $(this).text();//this is grabbing the text from the tags
             const url = $(this).find('a').attr('href');//this is getting the url that the tags lead to on click 
             articles.push({
@@ -23,5 +23,5 @@ axios(url)
         console.log(articles)
     }).catch(err => console.log(err))
 
-//app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));//listens to the port 
+app.listen(PORT, () => console.log(`server is running on PORT ${PORT}`));//listens to the port 
 
